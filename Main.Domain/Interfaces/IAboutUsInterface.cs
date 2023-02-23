e0fd493aac733c396babdff1bff8c6206e5fd2ac
shakiba <shakibafarhoodi@gmail.com>
2023-02-23 11:40:06 +0330

    CRUD About

---
+++ Main.Domain/Interfaces/IAboutUsInterface.cs
@@ -0,0 +1,31 @@
+﻿using Main.Domain.Models.AboutUs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.Domain.Interfaces
+{
+    public interface IAboutUsInterface
+    {
+        // list as AboutUsModel
+        // List<AboutUsModel> GetAll();
+
+        //aboutus id as model 
+        AboutUsModel GetAboutUs(int Id);
+        void AddAboutUs(AboutUsModel aboutUs);  
+
+        void EditAboutUs(AboutUsModel aboutUs); 
+        
+        void DetetAboutUs(AboutUsModel aboutUs);    
+
+        
+
+        
+        void save();
+        
+
+
+    }
+}
