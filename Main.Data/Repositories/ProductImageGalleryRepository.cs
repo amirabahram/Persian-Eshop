1fb9cb5ecf272c92c240a90102f18e65bfdd68b8
Amir Alaei <amiralaei70@gmail.com>
2023-03-15 15:22:37 +0330

    IsActive Added!

--- Main.Data/Repositories/ProductImageGalleryRepository.cs
+++ Main.Data/Repositories/ProductImageGalleryRepository.cs
@@ -1,4 +1,7 @@
-﻿using Main.Domain.Interfaces;
+﻿using Main.Data.Context;
+using Main.Domain.Interfaces;
+using Main.Domain.Models.Product_Image_Gallery;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +10,26 @@ using System.Threading.Tasks;
 
 namespace Main.Data.Repositories
 {
-    public class ProductImageGalleryRepository:IProductImageGalleryRepository
+    public class ProductImageGalleryRepository : IProductImageGalleryRepository
     {
+        private readonly EshopContext db;
+        public ProductImageGalleryRepository(EshopContext context)
+        {
+            this.db = context;
+        }
+        public async Task<List<ProductImageGallery>> GetAllImages()
+        {
+           return await db.ProductImageGalleries.Where(i => i.IsDelete == false).ToListAsync();
+        }
+
+        public async Task InsertImage(ProductImageGallery productImageGallery)
+        {
+            db.ProductImageGalleries.Add(productImageGallery);
+        }
+
+        public void UpdateImage(ProductImageGallery image)
+        {
+            db.ProductImageGalleries.Update(image);
+        }
     }
 }
--- Main.Domain/Interfaces/IProductImageGalleryRepository.cs
+++ Main.Domain/Interfaces/IProductImageGalleryRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Main.Domain.Models.Product_Image_Gallery;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,8 @@ namespace Main.Domain.Interfaces
 {
     public interface IProductImageGalleryRepository
     {
+        Task<List<ProductImageGallery>> GetAllImages();
+        Task InsertImage(ProductImageGallery productImageGallery);
+        void UpdateImage(ProductImageGallery image);
     }
 }
