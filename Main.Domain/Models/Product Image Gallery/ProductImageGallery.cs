00b7dce3f2db7104df0afbcbb75a60ceacc5486c
BTVRN <hamid.muzaffariafg1989@gmail.com>
2023-03-15 10:47:49 +0330

    create folder  and class for category,productImageGallery, product

---
+++ Main.Domain/Models/Product Image Gallery/ProductImageGallery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.Domain.Models.Product_Image_Gallery
+{
+    public class ProductImageGallery
+    {
+    }
+}
