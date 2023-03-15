e1ec18bc02d0de870828d1b2eff23222412d039a
Amir Alaei <amiralaei70@gmail.com>
2023-03-15 12:34:56 +0330

    gallery

--- Main.Domain/Models/Product Image Gallery/ProductImageGallery.cs
+++ Main.Domain/Models/Product Image Gallery/ProductImageGallery.cs
@@ -1,14 +1,18 @@
 ﻿using Main.Domain.Models.Common;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Main.Domain.Models.Product_Image_Gallery
 {
-    public class ProductImageGallery: BaseEntity
+    public class ProductImageGallery : BaseEntity
     {
-
+        public String? ImageName { get; set; }
+        [ForeignKey("ProductId")] //recommended
+        public int? ProductId { get; set; }
+        public Product.Product? Product { get; set; }
     }
 }
