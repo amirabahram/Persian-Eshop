12df2073de7e079353615997797d4999a323c3d2
BTVRN <hamid.muzaffariafg1989@gmail.com>
2023-03-15 11:50:27 +0330

    create product

--- Main.Domain/Models/Product/Product.cs
+++ Main.Domain/Models/Product/Product.cs
@@ -1,7 +1,11 @@
-﻿using Main.Domain.Models.Common;
+﻿
+using Main.Domain.Models.Category;
+using Main.Domain.Models.Common;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +14,17 @@ namespace Main.Domain.Models.Product
 {
     public class Product: BaseEntity
     {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public string MainImage { get; set; }
+        public int Count { get; set; }
 
+        public List<Product_Image_Gallery.ProductImageGallery>? productImageGalleries { get; set; }
+        [ForeignKey("Id")]
+        public Category.Category? category { get; set; }
+      
+      
+        
     }
 }
