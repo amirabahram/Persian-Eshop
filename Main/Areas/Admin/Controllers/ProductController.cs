92bc7976ccfd73e059cb02790362d3c0ed848e2d
Amir Alaei <amiralaei70@gmail.com>
2023-03-19 12:23:07 +0330

    cut product controller

--- Main/Areas/Admin/Controllers/ProductController.cs
+++ Main/Areas/Admin/Controllers/ProductController.cs
@@ -7,23 +7,6 @@ namespace Main.web.Areas.Admin.Controllers
 {
     public class ProductController : Controller
     {
-        private ICategoryService _categoryService;
-        public ProductController(ICategoryService categoryService)
-        {
-            this._categoryService = categoryService;
-        }
 
-        [Area("Admin")]
-        [HttpGet]
-        public IActionResult IndexCategory()
-        {
-            var category = _categoryService.GetAllCategories();
-            return View(category);
-        }
-        [HttpPost]
-        public IActionResult IndexCategory()
-        {
-            return View();
-        }
     }
 }
